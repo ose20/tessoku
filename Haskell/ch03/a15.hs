52b84ebe11c65fcead11d1ade0b80011fb03a811
Ayato Yokoyama <ayatoyokoyama@AyatonoMacBook-Pro.local>
2023-07-16 00:58:52 +0900

    update

--- Haskell/ch03/a15.hs
+++ Haskell/ch03/a15.hs
@@ -1,8 +1,11 @@
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE NumericUnderscores #-}
+{-# LANGUAGE BangPatterns #-}
 
 {-# OPTIONS_GHC -Wno-unused-imports #-}
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
+{-# OPTIONS_GHC -Wno-name-shadowing #-}
 
 import            Control.Monad (filterM, foldM, forM_, replicateM, when)
 import            Control.Monad.ST (ST, runST)
@@ -11,14 +14,44 @@ import            Data.Array.IArray (bounds, listArray, (!), IArray, accumArray,
 import            Data.Array.MArray (newArray, readArray, writeArray, freeze)
 import            Data.Array.ST (runSTUArray, STUArray)
 import            Data.Array.Unboxed (UArray)
-import            Data.Ix (Ix, inRange)
-import            Data.Sequence (Seq, Seq(Empty, (:<|)), (|>))
-import qualified  Data.Sequence as Seq
 import qualified  Data.ByteString.Char8 as BS
 import            Data.Char (isSpace)
-import            Data.List (unfoldr, foldl)
+import            Data.Ix (Ix, inRange)
+import            Data.List (unfoldr, foldl, sort)
 import            Data.List.Extra (chunksOf)
+import            Data.Sequence (Seq, Seq(Empty, (:<|)), (|>))
+import qualified  Data.Sequence as Seq
+import qualified  Data.Set as SET
+import qualified  Data.Vector.Algorithms.Intro as VAI
+import qualified  Data.Vector.Unboxed as VU
+import qualified  Data.Vector.Unboxed.Mutable as VUM
 
+
+
+nobs :: Ord a => [a] -> [a]
+nobs lst =
+  aux lst SET.empty []
+  where 
+    aux [] _ acc = reverse acc
+    aux (x:xs) set acc
+      | SET.member x set  = aux xs set acc
+      | otherwise         = aux xs (SET.insert x set) (x:acc)
+
+binSearch :: (Ord a, VU.Unbox a) => VU.Vector a -> a -> Int
+binSearch vec x =
+  loop 0 (VU.length vec)
+  where
+    loop left right
+      | vec VU.! mid == x = mid
+      | vec VU.! mid < x = loop mid right
+      | otherwise = loop left mid
+      where mid = (left + right) `div` 2
+
+compress :: (VU.Unbox a, Ord a) => [a] -> [Int]
+compress lst =
+  flip map lst $ \x -> binSearch sorted x + 1
+  where
+    sorted = VU.fromList $ sort $ nobs lst
 
 
 getInts :: IO [Int]
@@ -26,5 +59,8 @@ getInts = unfoldr (BS.readInt . BS.dropWhile isSpace) <$> BS.getLine
 
 main :: IO ()
 main = do
+  _ <- readLn @Int
+  as <- getInts
 
-  print 100
+  putStrLn $ unwords $ map show $ compress as
+
